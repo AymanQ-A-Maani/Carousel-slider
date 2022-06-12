616016a872a21ce3e1051be351d9b127e3449f50
AymanQ-A-Maani <=helldemon54@gmail.com>
2022-06-12 04:16:26 +0300

    fixed a bug where if you click very rapidly, the slider will display empty space

--- js/js.js
+++ js/js.js
@@ -11,7 +11,6 @@ let width = imgs[0].clientWidth;
 
 imgsDiv.style.transform = `translateX(${-width}px)`;
 bulletKeys[0].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
-console.log(currentpos);
 
 rArrow.addEventListener("click", ()=>{
     if(counter==1){
@@ -30,39 +29,50 @@ rArrow.addEventListener("click", ()=>{
         bulletKeys[2].style.backgroundColor = "rgb(173, 173, 173)"
     }
     counter++;
-    imgsDiv.style.transform = `translateX(${(-width * counter)}px)`
-    currentpos = (-width * counter)
-    imgsDiv.style.transition = `all .7s ease-in-out`
+    if(counter == 5){
+        counter =1
+    }else{
+        imgsDiv.style.transform = `translateX(${(-width * counter)}px)`
+        currentpos = (-width * counter)
+        imgsDiv.style.transition = `all .7s ease-in-out`
+    }
+    
 })
 
 lArrow.addEventListener("click",()=>{
-    counter--; 
-    if(counter==1){
-        bulletKeys[0].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
-        bulletKeys[2].style.backgroundColor = "rgb(173, 173, 173)"
-        bulletKeys[1].style.backgroundColor = "rgb(173, 173, 173)"
-        
-    }else if(counter ==2){
-        bulletKeys[1].style.backgroundColor ="rgba(255, 255, 255, 0.986)"
-        bulletKeys[0].style.backgroundColor = "rgb(173, 173, 173)"
-        bulletKeys[2].style.backgroundColor = "rgb(173, 173, 173)"
-
-    }else if(counter ==3){
-        bulletKeys[2].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
-        bulletKeys[1].style.backgroundColor = "rgb(173, 173, 173)"
-        bulletKeys[0].style.backgroundColor = "rgb(173, 173, 173)"
-    }
-    if(counter==0){
-        imgsDiv.style.transition = `all .7s ease-in-out`
-        imgsDiv.style.transform = `translateX(0px)`
-        bulletKeys[2].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
-        bulletKeys[1].style.backgroundColor = "rgb(173, 173, 173)"
-        bulletKeys[0].style.backgroundColor = "rgb(173, 173, 173)"
+    counter--;
+    if(counter==-1){
+        counter=1
     }else{
-        imgsDiv.style.transition = `all .7s ease-in-out`
-        imgsDiv.style.transform = `translateX(${currentpos + width}px)`
-        currentpos = currentpos + width
+        if(counter==1){
+            bulletKeys[0].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
+            bulletKeys[2].style.backgroundColor = "rgb(173, 173, 173)"
+            bulletKeys[1].style.backgroundColor = "rgb(173, 173, 173)"
+            
+        }else if(counter ==2){
+            bulletKeys[1].style.backgroundColor ="rgba(255, 255, 255, 0.986)"
+            bulletKeys[0].style.backgroundColor = "rgb(173, 173, 173)"
+            bulletKeys[2].style.backgroundColor = "rgb(173, 173, 173)"
+    
+        }else if(counter ==3){
+            bulletKeys[2].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
+            bulletKeys[1].style.backgroundColor = "rgb(173, 173, 173)"
+            bulletKeys[0].style.backgroundColor = "rgb(173, 173, 173)"
+        }
+        if(counter==0){
+            imgsDiv.style.transition = `all .7s ease-in-out`
+            imgsDiv.style.transform = `translateX(0px)`
+            bulletKeys[2].style.backgroundColor = "rgba(255, 255, 255, 0.986)"
+            bulletKeys[1].style.backgroundColor = "rgb(173, 173, 173)"
+            bulletKeys[0].style.backgroundColor = "rgb(173, 173, 173)"
+        }else{
+            imgsDiv.style.transition = `all .7s ease-in-out`
+            imgsDiv.style.transform = `translateX(${currentpos + width}px)`
+            currentpos = currentpos + width
+        }
+
     }
+    
 })
 
 imgsDiv.addEventListener("transitionend",()=>{
